652a50a1126f7d106259bc61125e389da7bae658
HitenCyber <hiten.dalsaniya@cybersurfindia.com>
2023-12-25 19:02:08 +0530

    First commit

---
+++ FastKart/src/main/java/com/FastKart/Repository/CouponRepository.java
@@ -0,0 +1,11 @@
+package com.FastKart.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.FastKart.entities.Coupon;
+
+public interface CouponRepository extends JpaRepository<Coupon, Integer > {
+
+	
+	
+}
